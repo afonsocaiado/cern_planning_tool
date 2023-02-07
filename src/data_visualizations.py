e3fa698c00b9769384061580ea9287fa70801179
Afonso Maria Rebordao Caiado De Sousa <afonso.maria.rebordao.caiado.de.sousa@cern.ch>
2023-02-07 17:23:26 +0100

    Started data visualizations

---
+++ src/data_visualizations.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+activity_list = pd.read_csv('activity_list.csv', encoding='unicode_escape')
+
+# Dataset information
+# Dataframe shape
+print("Shape: ", activity_list.shape)
+
+# Info about the DataFrame
+print("\nInfo:")
+print(activity_list.info())
+
+# Descriptive statistics of the numerical columns
+print("\nDescriptive Statistics:")
+print(activity_list.describe())
+
+# Number of unique values per column
+print("\nUnique values:")
+print(activity_list.nunique())
+
+# PLAN ID, UUID, NAME, VERSION ID, VERSION UUID, VERSION all the same = probably irrelevant
