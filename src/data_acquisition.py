7e4a27fcc3ae0cdb3a9ad88d7573a09284cd71b3
Afonso Maria Rebordao Caiado De Sousa <afonso.maria.rebordao.caiado.de.sousa@cern.ch>
2023-02-14 17:12:23 +0100

    q1 csv done with period names, not tested because plan is down

--- src/data_acquisition.py
+++ src/data_acquisition.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 activity_list = pd.read_csv('..\data\original\\activity_list.csv', encoding='unicode_escape')
 activity = pd.read_csv('..\data\original\\activity.csv', encoding='unicode_escape')
@@ -39,10 +40,10 @@ activity = activity.filter(items=['ID', 'LOCATION_INFORMATION', 'GOAL', 'IMPACT_
 
 # SCHEDULE
 # PHASE
-# phase = phase.filter(items=['ID', 'NAME_EN'])
-# phase.rename(columns={'ID': 'PHASE_UUID', 'NAME_EN': 'PHASE_NAME'}, inplace=True)
+phase = phase.filter(items=['ID', 'NAME_EN'])
+phase.rename(columns={'ID': 'PHASE_UUID', 'NAME_EN': 'PHASE_NAME'}, inplace=True)
 # HL SCHEDULABLE PHASE
-hl_schedulable_phase = hl_schedulable_phase.drop(columns=['SCHEDULE_ID', 'PHASE_UUID'])
+hl_schedulable_phase = hl_schedulable_phase.drop(columns=['SCHEDULE_ID'])
 hl_schedulable_phase.rename(columns={'ID': 'SCHEDULABLE_PHASE_UUID', 'AMOUNT': 'PHASE_AMOUNT', 'DURATION': 'PHASE_DURATION'}, inplace=True)
 # PERIOD
 period = period.drop(columns=['PERIOD_ID', 'PLAN_UUID', 'START_DATE', 'END_DATE', 'ACTIVE', 'PERIOD_ORDER', 'PARENT_UUID'])
@@ -50,16 +51,19 @@ period.rename(columns={'ID': 'PERIOD_UUID'}, inplace=True)
 proposed_periods = hl_sched_prop_period.merge(period, on='PERIOD_UUID', how='left')
 proposed_periods = proposed_periods.drop(columns=['PERIOD_UUID'])
 # Merging schedule phases and periods
+hl_schedulable_phase = hl_schedulable_phase.merge(phase, on='PHASE_UUID', how='left')
+hl_schedulable_phase = hl_schedulable_phase.drop(columns=['PHASE_UUID'])
 schedule_phases_periods = hl_schedulable_phase.merge(proposed_periods, on='SCHEDULABLE_PHASE_UUID', how='left')
 grouped_schedule_phases_periods = schedule_phases_periods.groupby('SCHEDULABLE_PHASE_UUID').agg({
     'ACTIVITY_UUID': 'first',
     'PHASE_AMOUNT': 'first',
     'PHASE_DURATION': 'first',
-    'NAME': lambda x: x.tolist()})   
+    'NAME': lambda x: x.tolist(),
+    'PHASE_NAME': 'first'})   
 grouped_schedule_phases_periods = grouped_schedule_phases_periods.reset_index()
 grouped_schedule_phases_periods = grouped_schedule_phases_periods.drop(columns=['SCHEDULABLE_PHASE_UUID'])
 
-grouped_merged_schedule_phase_phase = hl_schedulable_phase.groupby('ACTIVITY_UUID').agg(lambda x: x.tolist()) # grouping information by activities
+grouped_merged_schedule_phase_phase = grouped_schedule_phases_periods.groupby('ACTIVITY_UUID').agg(lambda x: x.tolist()) # grouping information by activities
 grouped_merged_schedule_phase_phase = grouped_merged_schedule_phase_phase.reset_index()
 grouped_merged_schedule_phase_phase['NEW_AMOUNT'] = grouped_merged_schedule_phase_phase['PHASE_AMOUNT'].copy()
 
@@ -71,8 +75,6 @@ for i, (dur, amt) in enumerate(zip(grouped_merged_schedule_phase_phase['PHASE_DU
 grouped_merged_schedule_phase_phase = grouped_merged_schedule_phase_phase.drop(columns=['PHASE_AMOUNT', 'PHASE_DURATION'])
 grouped_merged_schedule_phase_phase = grouped_merged_schedule_phase_phase.rename(columns={'NEW_AMOUNT': 'PHASE_AMOUNT'})
 
-
-
 # Q1 forming
 # Merge activiy and activity list tables
 merged_activity_list_activity = activity_list.merge(activity, on='ID', how='left')
@@ -81,9 +83,29 @@ merged_activity_list_activity.rename(columns={'ID': 'ACTIVITY_UUID'}, inplace=Tr
 # Merge total activity and schedule info
 q1 = merged_activity_list_activity.merge(grouped_merged_schedule_phase_phase, on='ACTIVITY_UUID', how='left')
 
+# Final treatment (bad practice)(last resort)
+def rearrange(arr):
+    if arr[0] == "Preparation" and arr[1] == "Commissioning":
+        return [arr[0], arr[2], arr[1]]
+    elif arr[0] == "Installation" and arr[1] == "Preparation":
+        return [arr[1], arr[0], arr[2]]
+    elif arr[0] == "Installation" and arr[1] == "Commissioning":
+        return [arr[2], arr[0], arr[1]]
+    elif arr[0] == "Commissioning" and arr[1] == "Preparation":
+        return [arr[1], arr[2], arr[0]]
+    elif arr[0] == "Commissioning" and arr[1] == "Installation":
+        return [arr[2], arr[1], arr[0]]
+    else:
+        return arr
+
+q1['PHASE_NAME_1'] = q1['PHASE_NAME'].apply(rearrange)
+q1['PHASE_AMOUNT_1'] = q1.apply(lambda row: [row['PHASE_AMOUNT'][row['PHASE_NAME'].index(val)] for val in row['PHASE_NAME_1']], axis=1)
+q1['NAME_1'] = q1.apply(lambda row: [row['NAME'][row['PHASE_NAME'].index(val)] for val in row['PHASE_NAME_1']], axis=1)
+q1 = q1.drop(columns=['PHASE_NAME', 'PHASE_AMOUNT', 'NAME', 'PHASE_NAME_1'])
+q1 = q1.rename(columns={'PHASE_NAME_1': 'PHASE_NAME', 'PHASE_AMOUNT_1': 'PHASE_AMOUNT', 'NAME_1': 'PERIOD'})
 q1 = q1.drop(columns=['ACTIVITY_VERSION'])
 
-print("\n", q1.head())
+print("\n", q1.head(10))
 print("Shape: ", q1.shape)
 #print(grouped_schedule_phases_periods.nunique())
 
