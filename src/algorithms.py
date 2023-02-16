cb84c395d967d72827d5db134dda4aab7ea0c070
Afonso Maria Rebordao Caiado De Sousa <afonso.maria.rebordao.caiado.de.sousa@cern.ch>
2023-02-16 10:39:21 +0100

    more

--- src/algorithms.py
+++ src/algorithms.py
@@ -6,11 +6,24 @@ from sklearn.metrics import silhouette_score
 import data_prep
 
 def kmeans(df):
+
     data_prep.remove_nans(df)
 
     # MODEL BUILDING
     # Preparing data for model
     # Encode variables
     data_prep.encode(df)
+
     # Normalize the variables
-    return data_prep.normalize(df, "zscore")
+    df_norm = data_prep.normalize(df, "zscore")
+
+    # MODEL APPLYING
+    # Specify the number of clusters
+    k = 5
+    # Initialize and fit KMeans model
+    model = KMeans(n_clusters=k)
+    model.fit(df_norm)
+    # Get cluster assignments for each data point
+    return model.labels_
+
+
--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
-import data_prep
 import algorithms
 
 # Load data from CSV file
@@ -14,16 +13,7 @@ q1 = pd.read_csv('..\data\processed\\q1.csv', encoding='unicode_escape')
 X1 = q1[['GROUP_RESPONSIBLE_NAME', 'ACTIVITY_TYPE_EN', 'WBS_NODE_CODE', 'FACILITY_NAMES']] # Simple important categorical values
 X2 = q1[['GROUP_RESPONSIBLE_NAME', 'RESPONSIBLE_WITH_DETAILS', 'ACTIVITY_TYPE_EN', 'WBS_NODE_CODE', 'PRIORITY_EN' , 'FACILITY_NAMES']] # All categorical
 
-X1_norm = algorithms.kmeans(X1)
-
-# MODEL APPLYING
-# Specify the number of clusters
-k = 5
-# Initialize and fit KMeans model
-model = KMeans(n_clusters=k)
-model.fit(X1_norm)
-# Get cluster assignments for each data point
-labels = model.labels_
+labels = algorithms.kmeans(X1) 
 # Add cluster labels to the original data
 X1['CLUSTER'] = labels
 
