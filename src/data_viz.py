39d24587155931ec2b136cf43fec558cad2a5b99
Afonso Maria Rebordao Caiado De Sousa <afonso.maria.rebordao.caiado.de.sousa@cern.ch>
2023-02-22 09:11:32 +0100

    vizs

--- src/data_viz.py
+++ src/data_viz.py
@@ -11,20 +11,20 @@ print(q1.head(10))
 categorical_features = ['GROUP_RESPONSIBLE_NAME', 'RESPONSIBLE_WITH_DETAILS', 'ACTIVITY_TYPE_EN', 'WBS_NODE_CODE', 'FACILITY_NAMES', 'PRIORITY_EN']
 numerical_columns = ['PREPARATION_DURATION', 'INSTALLATION_DURATION', 'COMMISSIONING_DURATION']
 
-# for feature in categorical_features:
-#     plt.figure()
-#     sns.countplot(x=feature, data=q1)
-#     plt.xticks(rotation=90)
-#     plt.show()
+for feature in categorical_features:
+    plt.figure()
+    sns.countplot(x=feature, data=q1)
+    plt.xticks(rotation=90)
+    plt.show()
 
 
-# for column in numerical_columns:
-#     plt.figure()
-#     sns.histplot(x=column, data=q1, bins=20)
-#     plt.show()
+for column in numerical_columns:
+    plt.figure()
+    sns.histplot(x=column, data=q1, bins=20)
+    plt.show()
 
 # # Create a scatter plot
-# plt.scatter(df['ACTIVITY_TYPE_EN'], df['PREPARATION_DURATION'], alpha=0.5)
+# plt.scatter(q1['ACTIVITY_TYPE_EN'], q1['PREPARATION_DURATION'], alpha=0.5)
 
 # # Add axis labels and title
 # plt.xlabel('Activity Type')
@@ -36,3 +36,19 @@ numerical_columns = ['PREPARATION_DURATION', 'INSTALLATION_DURATION', 'COMMISSIO
 
 # # Show the plot
 # plt.show()
+
+# Create scatter plot of WBS_NODE_CODE vs INSTALLATION_DURATION
+sns.scatterplot(x='WBS_NODE_CODE', y='INSTALLATION_DURATION', data=q1)
+plt.show()
+
+# Create box plot of ACTIVITY_TYPE_EN vs PREPARATION_DURATION
+sns.boxplot(x='ACTIVITY_TYPE_EN', y='PREPARATION_DURATION', data=q1)
+plt.show()
+sns.boxplot(x='ACTIVITY_TYPE_EN', y='INSTALLATION_DURATION', data=q1)
+plt.show()
+sns.boxplot(x='ACTIVITY_TYPE_EN', y='COMMISSIONING_DURATION', data=q1)
+plt.show()
+
+# Create histogram of COMMISSIONING_DURATION
+sns.histplot(data=q1, x='COMMISSIONING_DURATION', kde=True)
+plt.show()
